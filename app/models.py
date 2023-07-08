74f7dafd89afe6b881d696172217092608e83f06
20120511 <110280412+khoamajik1752@users.noreply.github.com>
2023-07-08 18:25:09 +0700

    migrate

--- app/models.py
+++ app/models.py
@@ -5,6 +5,6 @@ class Product(models.Model):
     id=models.IntegerField(primary_key=True)
     image=models.CharField(max_length=200)
     name=models.CharField(max_length=50)
-    description=models.CharField(max_length=100)
+    description=models.CharField(max_length=200)
     price=models.FloatField()
     color=models.CharField(max_length=10)
